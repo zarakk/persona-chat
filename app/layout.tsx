4e648ba68d12a03f97e97fcd58378a3cb2aa6af4
Zarak Khan <zedd0099@gmail.com>
2023-09-05 16:21:53 +0500

    feat: add user authentication via clerk

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+// const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body>{children}</body>
+      </html>
+    </ClerkProvider>
+  );
+}
