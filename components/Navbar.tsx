c53b507bae44614e28f923698f2abd486a56be8c
Zarak Khan <zedd0099@gmail.com>
2023-09-07 03:07:08 +0500

    feat: add sidebar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,7 +15,7 @@ export const Navbar = () => {
   return (
     <div
       className="fixed w-full z-50 flex justify-between 
-  items-center py-2 px-4 border-b border-primary/10 bg-secondary"
+  items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16"
     >
       <div className="flex items-center">
         <Menu className="block md:hidden" />
