29fabf437b9a5faa20fa73bba4cffa92b4313668
Zarak Khan <zedd0099@gmail.com>
2023-09-07 15:40:00 +0500

    feat: connect prismadb

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,15 @@
+import { Categories } from "@/components/Categories";
 import { Navbar } from "@/components/Navbar";
 import { SearchInput } from "@/components/SearchInput";
+import prismadb from "@/lib/prismadb";
 
-const Main = ({ children }: { children: React.ReactNode }) => {
+const Main = async () => {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
       <SearchInput />
+      <Categories data={categories} />
     </div>
   );
 };
