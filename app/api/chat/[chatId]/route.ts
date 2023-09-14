b8625d3d06bffc41842ea0867c8760a3ace795d0
Zarak Khan <zedd0099@gmail.com>
2023-09-14 16:57:47 +0500

    add ai langchain to backend

---
+++ app/api/chat/[chatId]/route.ts
@@ -0,0 +1,145 @@
+import { MemoryManager } from "@/lib/memory";
+import prismadb from "@/lib/prismadb";
+import { rateLimit } from "@/lib/rate-limit";
+import { auth, currentUser } from "@clerk/nextjs";
+import { LangChainStream, ReplicateStream, StreamingTextResponse } from "ai";
+import { CallbackManager } from "langchain/dist/callbacks/manager";
+import { Replicate } from "langchain/llms/replicate";
+
+import { NextResponse } from "next/server";
+
+export async function POST(
+  request: Request,
+  { params }: { params: { chatId: string } }
+) {
+  try {
+    const { prompt } = await request.json();
+    const user = await currentUser();
+    if (!user || !user.firstName || !user.id) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    const identifier = request.url + "-" + user.id;
+    const { success } = await rateLimit(identifier);
+
+    if (!success) {
+      return new NextResponse("Rate limit exceeded", { status: 429 });
+    }
+
+    const persona = await prismadb.persona.update({
+      where: {
+        id: params.chatId,
+      },
+      data: {
+        messages: {
+          create: {
+            content: prompt,
+            role: "user",
+            userId: user.id,
+          },
+        },
+      },
+    });
+
+    if (!persona) {
+      return new NextResponse("Persona not found", { status: 404 });
+    }
+
+    const name = persona.id;
+    const persona_file_name = name + ".txt";
+    const personaKey = {
+      personaName: name,
+      userId: user.id,
+      modelName: "llama2-13b",
+    };
+
+    const memoryManager = await MemoryManager.getInstance();
+
+    const records = await memoryManager.readLatestHistory(personaKey);
+
+    if (records.length === 0) {
+      await memoryManager.seedChatHistory(persona.seed, "\n\n", personaKey);
+    }
+
+    await memoryManager.writeToHistory("User: " + prompt + "\n", personaKey);
+
+    const recentChatHistory = await memoryManager.readLatestHistory(personaKey);
+
+    const similarDocs = await memoryManager.vectorSearch(
+      recentChatHistory,
+      persona_file_name
+    );
+
+    let relavantHistory = "";
+
+    if (!!similarDocs && similarDocs.length !== 0) {
+      relavantHistory = similarDocs.map((doc) => doc.pageContent).join("\n");
+    }
+
+    const { handlers } = LangChainStream();
+
+    const model = new Replicate({
+      model:
+        "a16z-infra/llama-2-13b-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5",
+      input: {
+        max_length: 2048,
+      },
+      apiKey: process.env.REPLICATE_API_TOKEN,
+      callbackManager: CallbackManager.fromHandlers(handlers),
+    });
+
+    model.verbose = true;
+
+    const resp = String(
+      await model
+        .call(
+          `
+          ONLY generate plain sentences without prefix of who is speaking. DO NOT use ${persona.name}: prefix. 
+  
+          ${persona.instructions}
+  
+          Below are relevant details about ${persona.name}'s past and the conversation you are in.
+          ${relavantHistory}
+  
+  
+          ${recentChatHistory}\n${persona.name}:`
+        )
+        .catch(console.error)
+    );
+
+    const cleaned = resp.replaceAll(",", "");
+    const chunks = cleaned.split("\n");
+    const response = chunks[0];
+
+    await memoryManager.writeToHistory("" + response.trim(), personaKey);
+
+    var Readable = require("stream").Readable;
+
+    let s = new Readable();
+    s.push(response);
+    s.push(null);
+
+    if (response !== undefined && response.length > 1) {
+      memoryManager.writeToHistory("" + response.trim(), personaKey);
+
+      await prismadb.persona.update({
+        where: {
+          id: params.chatId,
+        },
+        data: {
+          messages: {
+            create: {
+              content: response.trim(),
+              role: "system",
+              userId: user.id,
+            },
+          },
+        },
+      });
+    }
+
+    return new StreamingTextResponse(s);
+  } catch (error) {
+    console.log("[CHAT_POST]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
--- components/ChatMessages.tsx
+++ components/ChatMessages.tsx
@@ -2,7 +2,7 @@
 
 import { Persona } from "@prisma/client";
 import ChatMessage, { ChatMessageProps } from "./ChatMessage";
-import { useEffect, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 
 interface ChatMessagesProps {
   messages: ChatMessageProps[];
@@ -14,6 +14,8 @@ const ChatMessages = ({
   isLoading,
   persona,
 }: ChatMessagesProps) => {
+  const scrollRef = useRef<ElementRef<"div">>(null);
+
   const [fakeLoading, setFakeLoading] = useState(
     messages.length === 0 ? true : false
   );
@@ -27,6 +29,10 @@ const ChatMessages = ({
       clearTimeout(timeout);
     };
   }, []);
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
   return (
     <div className="flex-1 overflow-y-auto pr-4">
       <ChatMessage
@@ -35,6 +41,16 @@ const ChatMessages = ({
         role="system"
         content={`Hello, I am ${persona.name}, ${persona.description}`}
       />
+      {messages.map((message) => (
+        <ChatMessage
+          key={message.content}
+          role={message.role}
+          content={message.content}
+          src={message.src}
+        />
+      ))}
+      {isLoading && <ChatMessage role="system" src={persona.src} isLoading />}
+      <div ref={scrollRef} />
     </div>
   );
 };
