351f4520286cc171a8ed6b6fe43b94db5ec240a8
Zarak Khan <zedd0099@gmail.com>
2023-09-15 21:57:54 +0500

    fix: remove ui bugs

--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -1,15 +1,18 @@
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { Sidebar } from "./Sidebar";
-
-export const MobileSidebar = () => {
+import { useProModal } from "@/hooks/use-pro-modal";
+interface MobileSidebarProps {
+  isPro: boolean;
+}
+export const MobileSidebar = ({ isPro }: MobileSidebarProps) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
--- components/ProModal.tsx
+++ components/ProModal.tsx
@@ -11,14 +11,17 @@ import {
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const ProModal = () => {
   const proModal = useProModal();
   const { toast } = useToast();
   const [loading, setLoading] = useState(false);
-
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
   const onSubscribe = async () => {
     try {
       setLoading(true);
@@ -31,6 +34,10 @@ export const ProModal = () => {
       setLoading(false);
     }
   };
+
+  if (!isMounted) {
+    return null;
+  }
   return (
     <Dialog open={proModal.isOpen} onOpenChange={proModal.onClose}>
       <DialogContent>
